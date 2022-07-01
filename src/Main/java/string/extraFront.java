6793596878fdbb1d644ab88eea0955294e67a3ca
jaz <jasdeep.s94@gmail.com>
2022-07-01 07:13:55 -0400

    adding extraFront

---
+++ src/Main/java/string/extraFront.java
@@ -0,0 +1,20 @@
+package string;
+
+public class extraFront {
+
+    // Given a string, return a new string made of 3 copies of the first 2 chars of the original string.
+    // The string may be any length. If there are fewer than 2 chars, use whatever is there.
+    //
+    // extraFront("Hello") → "HeHeHe"
+    // extraFront("ab") → "ababab"
+    // extraFront("H") → "HHH"
+    public String extraFront(String str) {
+        String a;
+        if(str.length() >2) {
+            a = str.substring(0,2);
+            return new String(new char[3]).replace("\0", a);
+        }
+        return new String(new char[3]).replace("\0", str);
+    }
+
+}
